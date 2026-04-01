1891c61eec0c33f1312fc05f8112c85c5b1ff911
dowonk <31142896+dowonk@users.noreply.github.com>
2026-04-01 13:56:08 -0700

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 import webserver
 import requests
 import discord
-from discord.ext import commands
+from discord.ext import commands, tasks
 
 intents = discord.Intents.default()
 intents.message_content = True
